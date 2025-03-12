0374273c0d94b94122768a3f2cb79dbdcb1e47dd
KaiSed <121823522+kaised9@users.noreply.github.com>
2025-03-12 12:24:55 +0600

    Update N_Queen_Problem_Code.py
    Here I Uploaded Code Of N Queen Problem.

--- N_Queen_Problem/N_Queen_Problem_Code.py
+++ N_Queen_Problem/N_Queen_Problem_Code.py
@@ -1,1 +1,46 @@
+def isSafe(board,row,col,N):
 
+  for i in range(col):
+    if board[row][i]==1:
+      return False
+
+  for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
+    if board[i][j]==1:
+      return False
+
+  for i,j in zip(range(row,N,1),range(col,-1,-1)):
+    if board[i][j]==1:
+      return False
+  return True
+
+def solveNQUtility(board,col,N):
+  if col >= N:
+    return True
+
+  for i in range (N):
+    if isSafe(board,i,col,N):
+      board[i][col]=1
+
+      if solveNQUtility(board,col+1,N):
+        return True
+      board[i][col]=0
+
+  return False
+
+def printSolution(N):
+  board=[[0]*N for _ in range(N)]
+
+  if not solveNQUtility(board,0,N):
+    print("Solution Doesnot Exist !!!")
+    return False
+
+  print("Solution Found For",n,"Queens.")
+  for i in range(N):
+      for j in range(N):
+        print(board[i][j],end=" ")
+      print()
+  return True
+
+
+n=int (input("Enter Number Of Queen = "))
+printSolution(n)
